a891daa597260ee3995c36323855544e3a61094a
TrudesEmerald <getrudekyeikbh@gmail.com>
2021-12-05 10:55:47 +0000

    Team member section markup with responsiveness

--- js/main.js
+++ js/main.js
@@ -28,4 +28,29 @@ loop: true,
       });
 
 
- });      
+    
+
+
+    $('#team-members').owlCarousel({
+        items:3,
+        autoplay:true,
+        smartSpeed:750,
+        loop:true,
+        autoplayHoverPause:true,
+          responsive:{
+          0:{
+              items:1
+          },
+
+          480:{
+            items:2
+        },
+          768:{
+            items:3
+        }
+    }
+
+
+    });
+  });
+  
